1d3f37cc01cbc27e476cf0bc1fe9106781a22cfd
Ben Jenkyn <jenkynb@gmail.com>
2021-12-09 23:08:50 -0500

    formatting

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,65 +1,58 @@
-import dotenv from 'dotenv';
-dotenv.config()
+require("dotenv").config();
 
-const apiKey = process.env.WEATHER_API_KEY;
+const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
 
 export interface WeatherResponse {
   coord: {
-    lon: number,
-    lat: number
-  },
+    lon: number;
+    lat: number;
+  };
   weather: [
     {
-      id: number,
-      main: string,
-      description: string,
-      icon: string
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
     }
-  ],
-  base: string,
+  ];
+  base: string;
   main: {
-    temp: number,
-    feels_like: number,
-    temp_min: number,
-    temp_max: number,
-    pressure: number,
-    humidity: number
-  },
-  visibility: number,
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  visibility: number;
   wind: {
-    speed: number,
-    deg: number
-  },
+    speed: number;
+    deg: number;
+  };
   clouds: {
-    all: number
-  },
-  dt: number,
+    all: number;
+  };
+  dt: number;
   sys: {
-    type: number,
-    id: number,
-    message: number,
-    country: string,
-    sunrise: number,
-    sunset: number
-  },
-  timezone: number,
-  id: number,
-  name: string,
-  cod: number
-}                         
-
-                        
-
-export const weatherApi = async (city: string): Promise<WeatherResponse | null> => {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-    try{
-        const weatherResponse = await fetch(url)
-        const weatherData = await weatherResponse.json();
-        return weatherData;
-    }
-    catch(e: any){
-        console.error('Error fetching weather: ', e);
-        return null;
-    }
+    type: number;
+    id: number;
+    message: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
 }
+
+export const weatherApi = async (
+  city: string
+): Promise<WeatherResponse | null> => {
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  const weatherResponse = await fetch(url);
+  const weatherData = await weatherResponse.json();
+  return weatherData;
+};
