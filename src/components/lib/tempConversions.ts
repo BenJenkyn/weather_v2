6765f4641823b5baad07d9771bc10ca68888393a
Ben Jenkyn <jenkynb@gmail.com>
2021-12-21 16:41:24 -0500

    created tempConversions file for conversion functions

---
+++ src/components/lib/tempConversions.ts
@@ -0,0 +1,22 @@
+type measurementUnit = 'celcius' | 'farenheit';
+
+export const toCelcius = (tempKalvin: number): number => {
+    const celcius = tempKalvin - 273.15;
+    return celcius;
+};
+  
+export const toFarenheit = (tempKalvin: number): number => {
+    const celcius = toCelcius(tempKalvin);
+    const farenheit = celcius * (9 / 5) + 32;
+    return farenheit;
+};
+
+export const getTemperature = (tempKalvin: number, tempMeasurement: measurementUnit = 'celcius'): number => {
+    if (tempMeasurement === 'celcius'){
+        return(toCelcius(tempKalvin))
+    }
+    if (tempMeasurement === 'farenheit'){
+        return(toFarenheit(tempKalvin))
+    }
+    return -10000
+}
