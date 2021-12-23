119e787bad712e7494c2ce10ed853a527f7c45ad
Ben Jenkyn <jenkynb@gmail.com>
2021-12-22 23:30:47 -0500

    adds weather block

--- src/components/TemperatureDisplay/index.tsx
+++ src/components/TemperatureDisplay/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { GridItem, Text } from "@chakra-ui/react";
-import { WeatherResponse } from "../../api";
-import { getTemperature } from "../lib/tempConversions";
+import React from 'react';
+import { GridItem, Flex, Text, Box } from '@chakra-ui/react';
+import { WeatherResponse } from '../../api';
+import { getTemperature } from '../lib/tempConversions';
 
 interface Props {
   weatherData?: WeatherResponse;
@@ -18,9 +18,33 @@ const TemperatureDisplay = (props: Props) => {
       borderWidth="2px"
       borderRadius="15px"
     >
-      <Text>
-        {weatherData ?  `The weather is: ${getTemperature(weatherData?.main.temp).toFixed(1)}` : "--"}
-      </Text>
+      <Flex>
+        <Box flex="1">
+          <Text fontSize="100" textAlign="center">
+            {weatherData
+              ? `${getTemperature(weatherData?.main.temp).toFixed(1)}`
+              : '--'}
+          </Text>
+          <Text fontSize="25" textAlign="center">
+            &deg;C
+          </Text>
+        </Box>
+        <Box flex="1" fontSize="24" margin="auto" textColor="#4a4a4a">
+          <Text>
+            Low:{' '}
+            {weatherData
+              ? `${getTemperature(weatherData?.main.temp_min).toFixed(1)}`
+              : '--'}
+          </Text>
+          <Text>
+            High:{' '}
+            {weatherData
+              ? `${getTemperature(weatherData?.main.temp_max).toFixed(1)}`
+              : '--'}
+          </Text>
+          <Text>{weatherData ? weatherData?.weather[0].main : ''}</Text>
+        </Box>
+      </Flex>
     </GridItem>
   );
 };
